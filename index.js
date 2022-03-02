02b9356d949359231520cb20804d0c6a2dd3daf9
CloudGamer131 <mitchbri001@slyon.us>
2022-03-02 06:54:12 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,93 @@
-const { keep_alive } = require("./keep_alive");
 const aoijs = require("aoi.js")
+const fs = require('fs');
 const bot = new aoijs.Bot({
-    token: "process.env.token",
-prefix: "bot_prefix",
-mobile: true, //True or false
+    token: "OTI1OTg4NDQ1NDc2NzUzNDc5.Yc1H8Q.qM4ppXkseSdWXFepJ-Djy7GqoDU",
+prefix: ["+", "$getServerVar[prefix]"],
+mobile: true,
+sharding: false, //true or false
+    shardAmount: 20,
+fetchInvites: true, //True or false
 
-intents: ["GUILDS", "GUILD_MESSAGES"]
+intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]
 })
+bot.joinCommand({ 
+ channel:"$getServerVar[WelcomeChannel]", 
+ code:`$title[1;$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[WelcomeTitle];{user};$username];{user.ping};<@$authorID>];{server};$serverName];{user.tag};$userTag[$authorID]]]
+ $description[1;
+ 
+ 
+ $replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[WelcomeMessage];{user};$username];{user.ping};<@$authorID>];{server};$serverName];{user.tag};$userTag[$authorID]]
+ 
+ 
+ 
+ ]
+ $color[1;RED]
+ $footer[1;Now $serverName Has $membersCount Members!;$serverIcon]
+ $addTimestamp[1]
+ 
+ $thumbnail[1;$userAvatar[$authorID]]`}) 
+bot.onJoin()
 
 bot.onMessage() //Allows to execute Commands
 
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd, "./commands/")
 
+const files = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+files.forEach(x => {
+ require(`./events/${x}`)(bot)
+});
+
+bot.interactionCommand({
+name: "meme", 
+prototype : 'slash',
+code: `$interactionReply[;{newEmbed:{title:$getObjectProperty[title]}{color:#FF0000}{image:$getObjectProperty[image]}{footer:👍 $getObjectProperty[upvotes] | 💬 $replaceText[$getObjectProperty[comments];;0;-1]}}]
+$createObject[$jsonRequest[https://api.popcatdev.repl.co/meme;]]`
+}) 
+
 /*
 bot.cmd is object of Collections where the command data will be stored
 "./commands/" is the path of folder where all the commands' code will be present
 */
 
+bot.status ({
+ text: "+help",
+ type: "LISTENING",
+ time: 10
+})
+
 bot.status({
-    text: "$serverCount servers!",
+text: "$serverCount Servers!", 
+type: "COMPETING",
+time: "12"
+})
+bot.status({
+    text: "$allMembersCount Members!",
     type: "WATCHING",
     time: 12
 })
 
-bot.status({
-  text: "TEXT",
-  type: "PLAYING", //can be PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
-  status: "idle", //can be online, idle, dnd, offline
-  time: 12
-})
-
-bot.status({
-text: "TEXT", 
-type: "STREAMING", 
-url: "URL"
-})
-
-//Ready Event
 bot.readyCommand({
-    channel: "",
-    code: `$log[Ready on $userTag[$clientID]]`
+  channel: '',
+  code: `$log[
+  ╭─━━━━━━━━━━━━━━━━━━─╮
+  Client: $userTag[$clientID]
+  Ping: $ping ms
+  Bot Creator: $username[$botOwnerID]#$discriminator[$botOwnerID]
+  Commands loaded: $commandsCount
+  ╰─━━━━━━━━━━━━━━━━━━─╯
+  ]`
 })
+
+bot.variables ({
+prefix: "+",
+WelcomeChannel: "",
+WelcomeMessage: "Welcome {user.ping} To {server}!",
+ WelcomeTitle:"Welcome {user.ping}",
+afk: "false",
+afkmsg: "none",
+authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkyNTk4ODQ0NTQ3Njc1MzQ3OSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjQ2MDcyOTEzfQ.nuNGYyemMCxra3V1kHCEjxenyR6WpBr4EpGQqhVl7-Q",
+  wallet: "0", //wallet money
+  bank: "0", //bank money
+  bio: "I am Just a Plain Human" //user bio for profile
+});
